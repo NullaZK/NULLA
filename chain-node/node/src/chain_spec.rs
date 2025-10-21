0f9ecb413b82da33e6d1c89291aef82aea02a965
Nulla <Nullaproject@proton.me>
2025-10-21 22:48:46 +0200

    Update chain specifications for NULLA network

--- chain-node/node/src/chain_spec.rs
+++ chain-node/node/src/chain_spec.rs
@@ -1,29 +1,41 @@
-use sc_service::ChainType;
+use sc_service::{ChainType, Properties};
 use nulla_runtime::WASM_BINARY;
 
 /// Specialized `ChainSpec`. This is a specialization of the general Substrate ChainSpec type.
 pub type ChainSpec = sc_service::GenericChainSpec;
 
 pub fn development_chain_spec() -> Result<ChainSpec, String> {
+	let mut props = Properties::new();
+	props.insert("tokenSymbol".into(), "NULLA".into());
+	props.insert("tokenDecimals".into(), 12.into());
+	props.insert("ss58Format".into(), 42.into());
+
 	Ok(ChainSpec::builder(
 		WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?,
 		None,
 	)
 	.with_name("NULLA Development")
-	.with_id("nulla_dev")
+	.with_id("dev")
 	.with_chain_type(ChainType::Development)
 	.with_genesis_config_preset_name(sp_genesis_builder::DEV_RUNTIME_PRESET)
+	.with_properties(props)
 	.build())
 }
 
 pub fn local_chain_spec() -> Result<ChainSpec, String> {
+	let mut props = Properties::new();
+	props.insert("tokenSymbol".into(), "NULLA".into());
+	props.insert("tokenDecimals".into(), 12.into());
+	props.insert("ss58Format".into(), 42.into());
+
 	Ok(ChainSpec::builder(
 		WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?,
 		None,
 	)
 	.with_name("NULLA Local Testnet")
-	.with_id("nulla_local")
+	.with_id("local_testnet")
 	.with_chain_type(ChainType::Local)
 	.with_genesis_config_preset_name(sp_genesis_builder::LOCAL_TESTNET_RUNTIME_PRESET)
+	.with_properties(props)
 	.build())
 }
